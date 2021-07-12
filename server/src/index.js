aa65ee665a707629ee82fa3ec6f71f91611205b3
jaayperez <justin@startrev.com>
2021-07-12 15:05:35 -0700

    Connect to data sources

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,23 @@ require('dotenv').config();
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 
-const server = new ApolloServer({ typeDefs });
+const { createStore } = require('./utils');
+
+
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store })
+  })
+});
 
 server.listen().then(() => {
   console.log(`
