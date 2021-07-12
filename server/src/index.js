a00abb4ad0e097162b813041814b5bfb9ad71035
jaayperez <justin@startrev.com>
2021-07-12 14:46:21 -0700

    Build a schema

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,13 @@
 require('dotenv').config();
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(() => {
+  console.log(`
+    🚀 Server ready!
+    Listening on port 4000
+    Explore at https://studio.apollographql.com/sandbox
+  `);
+});
